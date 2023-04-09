a1416a66f68da87239d754f01e9e4b9737290e74
Van Phong <phongckm93@gmail.com>
2023-04-09 13:12:17 +0000

    ok

--- lab04/main.cpp
+++ lab04/main.cpp
@@ -132,7 +132,17 @@ void counterNull(Node *p, int &count)
         counter(p->right, count);
     }
 }
-void total(Node *p, int &sum)
+void countSma(Node *p, int &count, int &n)
+{
+    if (p != NULL)
+    {
+        if (p->data < n)
+            count++;
+        countSma(p->left, count, n);
+        countSma(p->right, count, n);
+    }
+}
+int total(Node *p, int &sum)
 {
     if (p != NULL)
     {
@@ -140,8 +150,9 @@ void total(Node *p, int &sum)
         total(p->left, sum);
         total(p->right, sum);
     }
+    return sum;
 }
-void totalEven(Node *p, int &sum)
+int totalEven(Node *p, int &sum)
 {
     if (p != NULL)
     {
@@ -153,8 +164,9 @@ void totalEven(Node *p, int &sum)
         totalEven(p->left, sum);
         totalEven(p->right, sum);
     }
+    return sum;
 }
-void totalOdd(Node *p, int &sum)
+int totalOdd(Node *p, int &sum)
 {
     if (p != NULL)
     {
@@ -166,6 +178,7 @@ void totalOdd(Node *p, int &sum)
         totalOdd(p->left, sum);
         totalOdd(p->right, sum);
     }
+    return sum;
 }
 void inputManual(Tree &t)
 {
@@ -191,7 +204,7 @@ void randomValue(Tree &t)
     int n = 10 + rand() % 11;
     for (int i = 0; i < n; i++)
     {
-        int x = 385 + rand() % 1787;
+        int x = -385 + rand() % 1787;
         addNode(t, createNode(x));
     }
 }
@@ -209,18 +222,75 @@ void findNode(Tree &t)
         cout << "False" << endl;
     }
 }
+void removeNode(Tree &t, int node)
+{
+    Node *i = t.root;
+    while (i != NULL)
+    {
+        if (i->data == node)
+        {
+            if (i->left == NULL && i->right == NULL)
+            {
+                t.root = NULL;
+            }
+            else if (i->left == NULL)
+            {
+                Node *p = i->right;
+                delete (i);
+                i = p;
+            }
+            else if (i->right == NULL)
+            {
+                Node *p = i->left;
+                delete (i);
+                i = p;
+            }
+            else
+            {
+                Node *p = i->right;
+                Node *q = i->left;
+                Node *r = i->right;
+                delete (i);
+                i = p;
+                i = q;
+                i = r;
+            }
+        }
+    }
+}
+void maxMin(Node *p, int &min, int &max)
+{
+    if (p != NULL)
+    {
+        if (p->data > max)
+        {
+            max = p->data;
+        }
+        if (p->data < min)
+        {
+            min = p->data;
+        }
 
+        maxMin(p->left, min, max);
+        maxMin(p->right, min, max);
+    }
+}
 int main()
 {
     Tree t;
     createTree(t);
+    int max;
+    int min;
+    int count = 0;
     int n = 0;
     int sum = 0;
+    int sumOdd = 0;
+    int sumEven = 0;
     int choose;
     bool isExit = false;
     while (isExit == false)
     {
-        cout << "Xin hay chon menu \n 3.Nhap du lieu tu ban phim \n 4.Ham tao cay tu dong doan[-358,1358] ,so luong tao [10,20] \n 5.Duyet cay theo NLR,LNR,LRN \n 6.Tim kiem gia tri X \n 9.Dem toan bo \n 10.Dem so not la cua cay \n 17.Tinh tong cac not \n 18.Tinh tong cac not chan \n 19.Tinh tong cac not le"
+        cout << "Xin hay chon menu \n 3.Nhap du lieu tu ban phim \n 4.Ham tao cay tu dong doan[-358,1358] ,so luong tao [10,20] \n 5.Duyet cay theo NLR,LNR,LRN \n 6.Tim kiem gia tri X \n 9.Dem toan bo \n 10.Dem so not la cua cay \n 12.Dem so not nho hon gia tri X \n 17.Tinh tong cac not \n 18.Tinh tong cac not chan \n 19.Tinh tong cac not le \n 20.So sanh tong cac nut chan va tong nut le \n 21.Tinh max va min"
              << endl;
         cout << "Chon = ";
         cin >> choose;
@@ -266,25 +336,64 @@ int main()
             cout << n << endl;
             cout << "--------------------------------" << endl;
             break;
+        case 12:
+            count = 0;
+            cout << "Nhap so X = ";
+            cin >> n;
+            countSma(t.root, count, n);
+            cout << "-----------Ket Qua-----------" << endl;
+            cout << count << endl;
+            cout << "--------------------------------" << endl;
+            break;
         case 17:
             sum = 0;
             cout << "-----------Ket Qua-----------" << endl;
-            total(t.root, sum);
-            cout << sum << endl;
+            cout << total(t.root, sum) << endl;
             cout << "--------------------------------" << endl;
             break;
         case 18:
             sum = 0;
             cout << "-----------Ket Qua-----------" << endl;
-            totalEven(t.root, sum);
-            cout << sum << endl;
+            cout << totalEven(t.root, sum) << endl;
             cout << "--------------------------------" << endl;
             break;
         case 19:
             sum = 0;
             cout << "-----------Ket Qua-----------" << endl;
-            totalOdd(t.root, sum);
-            cout << sum << endl;
+            cout << totalOdd(t.root, sumOdd) << endl;
+            cout << "--------------------------------" << endl;
+            break;
+        case 20:
+            cout << "-----------Ket Qua-----------" << endl;
+            sumEven = 0;
+            sumOdd = 0;
+            totalEven(t.root, sumEven);
+            totalOdd(t.root, sumOdd);
+            if (sumEven > sumOdd)
+            {
+                cout << sumEven << " > " << sumOdd << endl;
+                cout << "tong nut chan lon hon tong nut le" << endl;
+            }
+            else if (sumEven < sumOdd)
+            {
+                cout << sumEven << " < " << sumOdd << endl;
+                cout << "tong nut chan nho hon tong nut le" << endl;
+            }
+            else
+            {
+                cout << sumEven << "=" << sumOdd << endl;
+                cout << "tong nut chan nut le bang nhau" << endl;
+            }
+
+            cout << "--------------------------------" << endl;
+            break;
+        case 21:
+            max = t.root->data;
+            min = t.root->data;
+            maxMin(t.root, min, max);
+            cout << "-----------Ket Qua-----------" << endl;
+            cout << "Min = " << min << endl;
+            cout << "Max = " << max << endl;
             cout << "--------------------------------" << endl;
             break;
         default:
